8de8a283e954a109cae0efb10f551448404feeb4
walt <q18291926121@163.com>
2022-11-21 03:49:09 +0800

    第二次提交

--- src/main/java/org/innerclass/InterOps.java
+++ src/main/java/org/innerclass/InterOps.java
@@ -3,6 +3,6 @@ package org.innerclass;
 public class InterOps implements Inter {
     @Override
     public void eat() {
-        System.out.println("要吃饭~");
+        System.out.println("动物吃饭");
     }
 }
